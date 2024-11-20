1d07c1f79946993bd6711144f6f4acd5e96a3e71
Marcos Tulio Rodrigues <marcostulio06@hotmail.com>
2024-11-19 22:11:03 -0300

    feat: add utils random name and avatar

---
+++ frontend/src/app/utils/generate-random-name.utils.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const nameGenerator = require('@afuggini/namegenerator')
+
+export const generateRandomName = () => {
+  return nameGenerator(" ");
+};
